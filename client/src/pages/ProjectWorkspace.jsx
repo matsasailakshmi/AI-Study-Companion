c2859f21298bf4bdcd566cf2ade39fd9716d4ffa
matsasat09 <n210735@rgutktn.ac.in>
2026-09-18 22:08:10 +0530

    Fix project assessment API URL

--- client/src/pages/ProjectWorkspace.jsx
+++ client/src/pages/ProjectWorkspace.jsx
@@ -10,7 +10,7 @@ import Recommendations from "../components/Recommendations";
 
 import "./ProjectWorkspace.css";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 /*
   =========================================================
